14a2b87e0fa94a85ef1d2eb4a131a2741f910ad1
Roman Birg <romanbirg@gmail.com>
2011-12-19 23:55:16 -0600

    change default sounds

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -35,8 +35,10 @@ PRODUCT_PACKAGES := \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+    ro.config.ringtone=Girtab.ogg \
+    ro.config.notification_sound=Proxima.ogg \
+    ro.config.alarm_alert=Cesium.ogg
+    
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
