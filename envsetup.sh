0782f360bd9a72f0d81bc7db60092de4432eff3d
Roman Birg <romanbirg@gmail.com>
2012-01-26 12:19:43 -0600

    Revert "don't add lunch combos from device/ folder"
    This reverts commit 614f0e769277167063224f4bad6da62f6a16bc31.

--- envsetup.sh
+++ envsetup.sh
@@ -1457,8 +1457,7 @@ if [ "x$SHELL" != "x/bin/bash" ]; then
 fi
 
 # Execute the contents of any vendorsetup.sh files we can find.
-# removed device/*/*/vendorsetup.sh
-for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh 2> /dev/null`
+for f in `/bin/ls vendor/*/vendorsetup.sh vendor/*/*/vendorsetup.sh device/*/*/vendorsetup.sh 2> /dev/null`
 
 do
     echo "including $f"
