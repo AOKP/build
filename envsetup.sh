cb9e33cc4e4125f7355be260f3446e7db94c3237
Twisted <twistedumbrella@gmail.com>
2012-02-01 13:40:03 -0500

    build: Cleanup nam configuration and enable by default
    Removed unnecessary conditional for non-ota packages. Integrated checks
    to make sure NAM_VARIANT did not override AOKP_BUILD and AOKP_BUILD did not
    override NAM_VARIANT.
    Added to all aokp builds, since the script will be available and will not
    interfere with devices that do not need it, but also to all htc
    devices, which will usually need it.
    Enabled the script by default, since it will not hurt devices that do
    not require the configuration.
    
    Change-Id: I5cd6a4388c91c05fcf23dbd8db993b78a47eca68

--- envsetup.sh
+++ envsetup.sh
@@ -59,16 +59,15 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^aokp_") ; then
        AOKP_BUILD=$(echo -n $1 | sed -e 's/^aokp_//g')
+       NAM_VARIANT=$(echo -n $1 | sed -e 's/^aokp_//g')
+    elif (echo -n $1 | grep -q -e "htc_") ; then
+       AOKP_BUILD=
+       NAM_VARIANT=$(echo -n $1)
     else
        AOKP_BUILD=
+       NAM_VARIANT=
     fi
     export AOKP_BUILD
-
-    if (echo -n $1 | grep -q -e "htc_") ; then
-        NAM_VARIANT=$(echo -n $1)
-    else
-        NAM_VARIANT=
-    fi
     export NAM_VARIANT
 
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
