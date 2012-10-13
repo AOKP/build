5ffb81e3d4ce716e4eb1164d096b342d145e1233
Chirayu Desai <chirayudesai1@gmail.com>
2012-10-13 13:05:29 +0530

    build-with-colors: moar colors
    - rename CL_PFX and CL_INS to better match the
      colors they show.
    - add more colors.
    
    Output of:
    - most host stuff is yellow
    - most target stuff is green
    - installing/copying files is cyan
    - import/export includes and notice files is cyan
    - bootimage/recoveryimage is cyan
    - and some more colors in many places ;)
    
    Change-Id: I5532afa4ba608e0a7c408516dc9f912f9ca389f7

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -440,7 +440,7 @@ $(cleantarget) : PRIVATE_CLEAN_FILES := \
     $(LOCAL_INSTALLED_MODULE) \
     $(intermediates)
 $(cleantarget)::
-	@echo "Clean: $(PRIVATE_MODULE)"
+	@echo -e ${CL_GRN}"Clean:"${CL_RST}" $(PRIVATE_MODULE)"
 	$(hide) rm -rf $(PRIVATE_CLEAN_FILES)
 
 ###########################################################
@@ -507,7 +507,7 @@ endif
 	$(copy-file-to-new-target)
 else
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE)
-	@echo -e ${CL_INS}"Install: $@"${CL_RST}
+	@echo -e ${CL_CYN}"Install: $@"${CL_RST}
 	$(copy-file-to-target-with-cp)
 endif
 
@@ -515,7 +515,7 @@ ifdef LOCAL_DEX_PREOPT
 installed_odex := $(basename $(LOCAL_INSTALLED_MODULE)).odex
 built_odex := $(basename $(LOCAL_BUILT_MODULE)).odex
 $(installed_odex) : $(built_odex) | $(ACP)
-	@echo -e ${CL_INS}"Install: $@"${CL_RST}
+	@echo -e ${CL_CYN}"Install: $@"${CL_RST}
 	$(copy-file-to-target)
 
 $(LOCAL_INSTALLED_MODULE) : $(installed_odex)
