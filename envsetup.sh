9302cf1d742cace05a36110a0f373c5b04c6086c
dhacker29 <davidhackerdvm@gmail.com>
2013-02-20 19:06:31 -0600

    Fix aokp make file inclusion logic
    This is a combination of 5 commits. Based on CM
    Revert "Revert "Load only the current product config makefile.""
    
    This reverts commit b6443c4e1f762af971d1d404645f8a08853a672b.
    
    build: Preliminary support for CM product builds
    
    If building a cm_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/cm/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for CM makefiles.
    
    build: Fix fastpath code for CM_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    
    Fix aokp make file inclusion logic
    
    Change-Id: I0505437c4503218c0519c434272bf76f59345d16
    
    PS2: Fix missing include
    PS3: change to AOKP_TARGET so it doesn't interfere with AOKP_BUILD variable

--- envsetup.sh
+++ envsetup.sh
@@ -59,6 +59,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^aokp_") ; then
+       AOKP_TARGET=$(echo -n $1 | sed -e 's/^aokp_//g')
+    else
+       AOKP_TARGET=
+    fi
+    export AOKP_TARGET
+
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
