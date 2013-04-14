18ed5a3a4b44407c647b6576ee7e42e5ec504cdc
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-04-14 12:13:23 +0200

    Secure adb connections on non-eng builds
    Change-Id: I9cbfa594d39d9921721e78f0864d21c9935e713c
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- core/main.mk
+++ core/main.mk
@@ -290,6 +290,9 @@ ifneq (,$(user_variant))
   # Target is secure in user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
+  # Secure adb connections
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
     tags_to_install += debug
