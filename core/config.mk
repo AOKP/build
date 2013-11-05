c3914bc2ba02b01b829552ec04901a2cb42826c6
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 22:17:59 +0000

    build: Fix global selinux policy inclusion
    We're adding specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I7989a1093f9abc8360d492e73a860eb4afad10ad

--- core/config.mk
+++ core/config.mk
@@ -491,4 +491,8 @@ RS_PREBUILT_CLCORE := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libclcore.bc
 RS_PREBUILT_LIBPATH := -L prebuilts/ndk/8/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
 RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcompiler_rt.a
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include vendor/aokp/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk
