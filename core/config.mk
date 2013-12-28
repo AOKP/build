d9c41f2e6f4ffaee6cea0770cad15093b8310c93
Steve Kondik <shade@chemlab.org>
2013-12-28 15:48:20 -0800

    build: Add QCOM target-specific config
     * Needed to support global DirectTract config on legacy targets
     * Let's also eliminate some boilerplate
    
    Change-Id: I736c10a5e7e1f3d1e0de9e60f29b60add276f151
    
    Conflicts:
    	core/config.mk

--- core/config.mk
+++ core/config.mk
@@ -491,6 +491,9 @@ RS_PREBUILT_CLCORE := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libclcore.bc
 RS_PREBUILT_LIBPATH := -L prebuilts/ndk/8/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
 RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcompiler_rt.a
 
+# Rules for QCOM targets
+include $(BUILD_SYSTEM)/qcom_target.mk
+
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/aokp/sepolicy/sepolicy.mk)
