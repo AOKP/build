6cc08a21015d06a2250ed1fae1a2f949e233c2d4
Andrew Warfield <hiasant@gmail.com>
2013-12-27 21:28:32 -0800

    build: no longer build emulator by default
    This is just increasing build times when most people are not using it.
    Let's speed up builds a bit.
    
    Building emulator should still be possible by appending BUILD_EMULATOR=true
    to the build command
    
    PS2: Let's keep the darwin warning.
    
    Change-Id: Ibd8e2b90455bd603a691c7b95466068b6fae212a
    Signed-off-by: Andrew Warfield <hiasant@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -186,6 +186,7 @@ $(info ************************************************************)
 $(error stop)
 endif
 
+# We're Unicorns. We use magic. Not emulators.
 ifndef BUILD_EMULATOR
 ifeq (darwin,$(HOST_OS))
 GCC_REALPATH = $(realpath $(shell which $(HOST_CC)))
@@ -198,11 +199,11 @@ ifneq ($(findstring llvm-gcc,$(GCC_REALPATH)),)
   $(warning ****************************************)
   BUILD_EMULATOR := false
 else
-  BUILD_EMULATOR := true
+  BUILD_EMULATOR := false
 endif
-else   # HOST_OS is not darwin
-  BUILD_EMULATOR := true
-endif  # HOST_OS is darwin
+else
+  BUILD_EMULATOR := false
+endif
 endif
 
 $(shell echo 'VERSIONS_CHECKED := $(VERSION_CHECK_SEQUENCE_NUMBER)' \
