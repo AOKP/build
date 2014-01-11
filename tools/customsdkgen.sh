322a547d46e38e04d468a52f086a1469a55da343
Arnav Gupta <championswimmer@aokp.co>
2014-01-11 05:56:26 +0530

    sdkgen: logging: coloured output and indication of completion
    Change-Id: Iba754e9c277bdcabab992f31905af964836059c4
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- tools/customsdkgen.sh
+++ tools/customsdkgen.sh
@@ -4,8 +4,10 @@ SDK_VER=19
 CUSTOM_VER=119
 CUSTOM_NAME=aokp
 
+. ${ANDROID_BUILD_TOP}/vendor/aokp/tools/colors
+
 if [ -z "$OUT" ]; then
-    echo "Please lunch a product before using this command"
+    echo -e $CL_RED"Please lunch a product before using this command"$CL_RST
     exit 1
 else
     OUTDIR=${OUT%/*/*/*}
@@ -43,14 +45,14 @@ $(cd ${TMP_DIR}; jar -xf ${TELEPHONYJAR})
 
 jar -cf ${OUTDIR}/android.jar -C ${TMP_DIR}/ .
 
-echo "android.jar created at ${OUTDIR}/android.jar"
-echo "Now attempting to create new sdk platform with it"
+echo -e $CL_GRN"android.jar created at ${OUTDIR}/android.jar"$CL_RST
+echo -e $CL_YLW"Now attempting to create new sdk platform with it"$CL_RST
 
 if [ -z "$ANDROID_HOME" ]; then
     ANDROID=$(command -v emulator)
     ANDROID_HOME=${ANDROID%/*}
     if [ -z "$ANDROID_HOME" ]; then
-        echo "ANDROID_HOME variable is not set. Do you have the sdk installed ?"
+        echo -e $CL_RED"ANDROID_HOME variable is not set. Do you have the sdk installed ?"$CL_RST
         exit 1
     fi
 fi
@@ -62,4 +64,8 @@ sed -i 's/^ro\.build\.version\.sdk=.*/ro.build.version.sdk=119/g' ${ANDROID_HOME
 sed -i 's/^ro\.build\.version\.release=.*/ro.build.version.release=4.4-aokp/g' ${ANDROID_HOME}/platforms/android-${SDK_VER}-${CUSTOM_NAME}/build.prop
 sed -i 's/AndroidVersion.ApiLevel=19/AndroidVersion.ApiLevel=119/' ${ANDROID_HOME}/platforms/android-${SDK_VER}-${CUSTOM_NAME}/source.properties
 sed -i 's/Pkg.Desc=/Pkg.Desc=AOKP /' ${ANDROID_HOME}/platforms/android-${SDK_VER}-${CUSTOM_NAME}/source.properties
+
+if [ -f ${ANDROID_HOME}/platforms/android-${SDK_VER}-${CUSTOM_NAME}/android.jar ]; then
+    echo -e $CL_CYN"New SDK platform with custom android.jar created inside ${ANDROID_HOME}"$CL_RST
+fi
 
