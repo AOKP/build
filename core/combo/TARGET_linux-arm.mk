3493e1b74d4e9bc8555071314483c8bf567a1adf
K_TECH <khusikadhamar@gmail.com>
2014-06-08 07:31:22 +0700

    core: arm: Add support for GCC 4.8 and 4.9
    source: https://github.com/AriesVE-DevCon-TEAM/android_build/commit/2a895ed5ca9b73bbc706bf19476b77a78226f52e

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -121,9 +121,11 @@ TARGET_GLOBAL_CFLAGS += \
 # We cannot turn it off blindly since the option is not available
 # in gcc-4.4.x.  We also want to disable sincos optimization globally
 # by turning off the builtin sin function.
-ifneq ($(filter 4.6 4.6.% 4.7 4.7.%, $(TARGET_GCC_VERSION)),)
+ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.8.% 4.9 4.9.%, $(TARGET_GCC_VERSION)),)
 TARGET_GLOBAL_CFLAGS += -Wno-unused-but-set-variable -fno-builtin-sin \
-			-fno-strict-volatile-bitfields
+			-fno-strict-volatile-bitfields \
+			-Wno-error=unused-parameter \
+			-Wno-error=unused-variable
 endif
 
 # This is to avoid the dreaded warning compiler message:
