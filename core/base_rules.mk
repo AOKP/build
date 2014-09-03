43cfaa5acd9c5d5cbf4750f82a88af87ea5c8714
Michael Bestas <mikeioannina@gmail.com>
2014-09-04 00:50:46 +0300

    Squashed commits for colored build output
    Original work from Chirayu Desai & Tanguy Pruvot
    Ported manually since cherry-picking was too hard
    
    Change-Id: Id067ce97b170c40a7b25895f88438163289c1ed2

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -466,7 +466,7 @@ $(cleantarget) : PRIVATE_CLEAN_FILES := \
     $(LOCAL_INSTALLED_MODULE) \
     $(intermediates)
 $(cleantarget)::
-	@echo "Clean: $(PRIVATE_MODULE)"
+	@echo -e ${CL_GRN}"Clean:"${CL_RST}" $(PRIVATE_MODULE)"
 	$(hide) rm -rf $(PRIVATE_CLEAN_FILES)
 
 ###########################################################
@@ -527,15 +527,15 @@ $(LOCAL_INSTALLED_MODULE): PRIVATE_POST_INSTALL_CMD := $(LOCAL_POST_INSTALL_CMD)
 ifneq ($(LOCAL_ACP_UNAVAILABLE),true)
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE) | $(ACP)
 ifeq ($(OUT_DIR),out)
-	@echo -e ${CL_INS}"Install: $(ANDROID_BUILD_TOP)/$@"${CL_RST}
+	@echo -e ${CL_CYN}"Install: $(ANDROID_BUILD_TOP)/$@"${CL_RST}
 else
-	@echo -e ${CL_INS}"Install: $@"${CL_RST}
+	@echo -e ${CL_CYN}"Install: $@"${CL_RST}
 endif
 	$(copy-file-to-new-target)
 	$(PRIVATE_POST_INSTALL_CMD)
 else
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE)
-	@echo -e ${CL_INS}"Install: $@"${CL_RST}
+	@echo -e ${CL_CYN}"Install: $@"${CL_RST}
 	$(copy-file-to-target-with-cp)
 endif
 
@@ -543,7 +543,7 @@ ifdef LOCAL_DEX_PREOPT
 installed_odex := $(basename $(LOCAL_INSTALLED_MODULE)).odex
 built_odex := $(basename $(LOCAL_BUILT_MODULE)).odex
 $(installed_odex) : $(built_odex) $(LOCAL_BUILT_MODULE) | $(ACP)
-	@echo "Install: $@"
+	@echo -e ${CL_CYN}"Install: $@"${CL_RST}
 	$(copy-file-to-target)
 
 $(LOCAL_INSTALLED_MODULE) : $(installed_odex)
