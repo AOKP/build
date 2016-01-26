d4533b6c2937b9d5e771b738cc21398f8dd25b01
STELIX <ssspinni@gmail.com>
2016-01-26 18:32:50 +0100

    Build - AOKPize the build [1/2]
    Change-Id: If482ba85e0868c8196c501f2a6c1ed7ede6a2bd0
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ core/colors.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2015 AOKP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Localized color definitions
+# tput color defs:
+CLR_GRN=$(shell tput setaf 2)
+CLR_YLW=$(shell tput setaf 3)
+CLR_BLU=$(shell tput setaf 4)
+CLR_MAG=$(shell tput setaf 5)
+CLR_CYN=$(shell tput setaf 6)
+CLR_WHT=$(shell tput setaf 7)
+CLR_BOLD=$(shell tput bold)
+CLR_RST=$(shell tput sgr0)
+# Shell color defs:
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+CL_YLW="\033[33m"
+CL_BLU="\033[34m"
+CL_MAG="\033[35m"
+CL_CYN="\033[36m"
+CL_RST="\033[0m"
