95296071d28dd8fbe78f67f2675024f7827816df
Arnav Gupta <championswimmer@gmail.com>
2016-02-02 17:13:23 +0000

    unicorndust: a roomservice clone for AOKP repos
    PS3 - Fetch from AOKP GitHub Before CyanogenMod incase device isn't present locally
    PS4 - Do not fetch new trees from CM at all
    PS5 - use 'aokp' and 'cm' specific remotes and not 'github'
    PS6 - fix more corner cases
    
    Change-Id: I03a3d73a97e06312f02fece945b684aebd679cef
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- tools/unicorndust.py
+++ tools/unicorndust.py
@@ -51,7 +51,7 @@ except:
     device = product
 
 if not depsonly:
-    print("Device %s not found. Attempting to retrieve device repository from CyanogenMod Github (http://github.com/CyanogenMod)." % device)
+    print("Device %s not found. Attempting to retrieve device repository from AOKP Github (http://github.com/AOKP)." % device)
 
 repositories = []
 
@@ -71,7 +71,7 @@ def add_auth(githubreq):
         githubreq.add_header("Authorization","Basic %s" % githubauth)
 
 if not depsonly:
-    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:CyanogenMod+in:name+fork:true" % device)
+    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:AOKP+in:name+fork:true" % device)
     add_auth(githubreq)
     try:
         result = json.loads(urllib.request.urlopen(githubreq).read().decode())
@@ -123,11 +123,8 @@ def get_from_manifest(devicename):
         lm = ElementTree.Element("manifest")
 
     for localpath in lm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
-            return localpath.get("path")
         if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
-
 
     # Devices originally from AOSP are in the main manifest...
     try:
@@ -137,7 +134,7 @@ def get_from_manifest(devicename):
         mm = ElementTree.Element("manifest")
 
     for localpath in mm.findall("project"):
-        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+        if re.search("device_.*_%s$" % device, localpath.get("name")):
             return localpath.get("path")
 
     return None
@@ -178,12 +175,12 @@ def add_to_manifest(repositories, fallback_branch = None):
         repo_target = repository['target_path']
         print('Checking if %s is fetched from %s' % (repo_target, repo_name))
         if is_in_manifest(repo_target):
-            print('CyanogenMod/%s already fetched to %s' % (repo_name, repo_target))
+            print('AOKP/%s already fetched to %s' % (repo_name, repo_target))
             continue
 
-        print('Adding dependency: CyanogenMod/%s -> %s' % (repo_name, repo_target))
+        print('Adding dependency: AOKP/%s -> %s' % (repo_name, repo_target))
         project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "cm", "name": "CyanogenMod/%s" % repo_name })
+            "remote": "aokp", "name": "AOKP/%s" % repo_name })
 
         if 'branch' in repository:
             project.set('revision',repository['branch'])
@@ -205,7 +202,7 @@ def add_to_manifest(repositories, fallback_branch = None):
 
 def fetch_dependencies(repo_path, fallback_branch = None):
     print('Looking for dependencies')
-    dependencies_path = repo_path + '/cm.dependencies'
+    dependencies_path = repo_path + '/aokp.dependencies'
     syncable_repos = []
 
     if os.path.exists(dependencies_path):
@@ -224,7 +221,7 @@ def fetch_dependencies(repo_path, fallback_branch = None):
             print('Adding dependencies to manifest')
             add_to_manifest(fetch_list, fallback_branch)
     else:
-        print('CM: Dependencies file not found, bailing out at ' + dependencies_path)
+        print('AOKP: Dependencies file not found, bailing out at' + dependencies_path)
 
     if len(syncable_repos) > 0:
         print('Syncing dependencies')
@@ -241,17 +238,17 @@ if depsonly:
     if repo_path:
         fetch_dependencies(repo_path)
     else:
-        print("CM: Trying dependencies-only mode on a non-existing device tree? " + repo_path)
+        print("AOKP: Trying dependencies-only mode on a non-existing device tree? " + repo_path)
 
     sys.exit()
 
 else:
     for repository in repositories:
         repo_name = repository['name']
-        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+        if repo_name.startswith("device_") and repo_name.endswith("_" + device):
             print("Found repository: %s" % repository['name'])
             
-            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+            manufacturer = repo_name.replace("device_", "").replace("_" + device, "")
             
             default_revision = get_default_revision()
             print("Default revision: %s" % default_revision)
@@ -297,4 +294,4 @@ else:
             print("Done")
             sys.exit()
 
-print("Repository for %s not found in the CyanogenMod Github repository list. If this is in error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
+print("Repository for %s not found in the AOKP Github repository list. If this is in error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
