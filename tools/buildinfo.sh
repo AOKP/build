ad6b7b59cd667f3f25d11290c6ce8b4c16549b82
Akhil Narang <akhil.narang@protonmail.com>
2016-02-08 20:59:18 +0530

    Build - buildinfo: remove duplicate prop
    - AOKP vendor repo already sets this in common.mk
    
    Change-Id: I3e91d1d1788d76d2aebf9607f7c20de7f4dd5dda
    Signed-off-by: Akhil Narang <akhil.narang@protonmail.com>

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -65,6 +65,4 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
-echo "ro.aokp.device=$AOKP_DEVICE"
-
 echo "# end build properties"
