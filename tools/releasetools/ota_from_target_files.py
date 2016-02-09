c0639724f90ae7bbe78f6d5c399c3d1cbfcd0168
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-02-08 23:56:47 -0500

    ota_from_target_files: Use ro.product.model instead of brand
    * This will be different per device instead of only per carrier/mfg.
    
    https://source.android.com/source/add-device.html#prod-def
    
    Change-Id: I64490d0dccaa492566bb70b4215703730249676f
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -659,7 +659,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("*   Compiled: %s"%(build));
 
   device = GetBuildProp("ro.aokp.device", OPTIONS.info_dict)
-  model = GetBuildProp("ro.product.brand", OPTIONS.info_dict)
+  model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
   script.Print("*   Device: %s (%s)"%(model, device));
   script.Print("******************************************");
 
