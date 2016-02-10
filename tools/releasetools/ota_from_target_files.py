e49be0ffa1f5f2f3d182ca58a2d6f8e002a5273a
Meninblack007 <sanyam.53jain@gmail.com>
2016-02-10 03:02:34 -0500

    ota_from_target_files: Use ro.product.name instead of model
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -659,8 +659,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("*   Compiled: %s"%(build));
 
   device = GetBuildProp("ro.aokp.device", OPTIONS.info_dict)
-  model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
-  script.Print("*   Device: %s (%s)"%(model, device));
+  name = GetBuildProp("ro.product.name", OPTIONS.info_dict)
+  script.Print("*   Device: %s (%s)"%(name, device));
   script.Print("******************************************");
 
   if OPTIONS.wipe_user_data:
