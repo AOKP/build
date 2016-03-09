e80c93e835fa37a10740992798b2f1200bb57bd9
Dark Eyes <dark_eyes_@hotmail.com>
2016-03-08 22:05:10 -0500

    Build - envsetup: Always look up JAVA_HOME path
    Change-Id: I8b0641eb107cd095079bb17f92e99730f532fda7

--- envsetup.sh
+++ envsetup.sh
@@ -1562,6 +1562,46 @@ function godir () {
     \cd $T/$pathname
 }
 
+# Force JAVA_HOME to point to java 1.7/1.8 if it isn't already set.
+function set_java_home() {
+    # Clear the existing JAVA_HOME value if we set it ourselves, so that
+    # we can reset it later, depending on the version of java the build
+    # system needs.
+    #
+    # If we don't do this, the JAVA_HOME value set by the first call to
+    # build/envsetup.sh will persist forever.
+    if [ -n "$ANDROID_SET_JAVA_HOME" ]; then
+      export JAVA_HOME=""
+    fi
+
+    if [ ! "$JAVA_HOME" ]; then
+      if [ -n "$LEGACY_USE_JAVA7" ]; then
+        echo Warning: Support for JDK 7 will be dropped. Switch to JDK 8.
+        case `uname -s` in
+            Darwin)
+                export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+                ;;
+            *)
+                export JAVA_HOME=$(dirname $(dirname $(dirname $(readlink -f $(which java)))))
+                ;;
+        esac
+      else
+        case `uname -s` in
+            Darwin)
+                export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+                ;;
+            *)
+                export JAVA_HOME=$(dirname $(dirname $(dirname $(readlink -f $(which java)))))
+                ;;
+        esac
+      fi
+
+      # Keep track of the fact that we set JAVA_HOME ourselves, so that
+      # we can change it on the next envsetup.sh, if required.
+      export ANDROID_SET_JAVA_HOME=true
+    fi
+}
+
 # Print colored exit condition
 function pez {
     "$@"
