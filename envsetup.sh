d90312e17a9899d0a48996200439f2857041fbd2
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-19 21:02:51 +0200

    build: Fix caf and aospremote
    Change-Id: I8ad28d71254941c0aacb91b1c6c702f9f13caf5f

--- envsetup.sh
+++ envsetup.sh
@@ -1813,7 +1813,7 @@ function aospremote()
         return 1
     fi
     git remote rm aosp 2> /dev/null
-    PROJECT=${$(pwd -P)#$ANDROID_BUILD_TOP/}
+    PROJECT=$(pwd -P | sed "s#$ANDROID_BUILD_TOP\/##")
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
@@ -1830,7 +1830,7 @@ function cafremote()
         return 1
     fi
     git remote rm caf 2> /dev/null
-    PROJECT=${$(pwd -P)#$ANDROID_BUILD_TOP/}
+    PROJECT=$(pwd -P | sed "s#$ANDROID_BUILD_TOP\/##")
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
