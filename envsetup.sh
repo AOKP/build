990d04af1f9b55c37beadead71734d2523800a1c
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-20 23:25:40 +0300

    envsetup: Fix cafremote/aospremote for projects with non-standard paths
    * Fix -caf and -caf-<platform> projects
    * Fix AOSP HALs that are synced in "/default" subfolder
    
    Change-Id: I33cc344a3234de6698676c32035622acbec03dd1

--- envsetup.sh
+++ envsetup.sh
@@ -1813,7 +1813,7 @@ function aospremote()
         return 1
     fi
     git remote rm aosp 2> /dev/null
-    PROJECT=$(pwd -P | sed "s#$ANDROID_BUILD_TOP\/##")
+    PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
@@ -1830,7 +1830,7 @@ function cafremote()
         return 1
     fi
     git remote rm caf 2> /dev/null
-    PROJECT=$(pwd -P | sed "s#$ANDROID_BUILD_TOP\/##")
+    PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
     if (echo $PROJECT | grep -qv "^device")
     then
         PFX="platform/"
