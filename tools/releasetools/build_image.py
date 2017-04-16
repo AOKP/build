25e74751c47ab62d96af786d2e381dadb0452e56
STELIX <ssspinni@gmail.com>
2017-04-16 12:40:32 +0200

    build - fix build after 7.1.2 r2 tag merge
    Change-Id: I7de74630c5d2dad51e422f720ae88b6529c332ef
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -154,7 +154,7 @@ def BuildVerityFEC(sparse_image_path, verity_path, verity_fec_path,
                    padding_size):
   cmd = "fec -e -p %d %s %s %s" % (padding_size, sparse_image_path,
                                    verity_path, verity_fec_path)
-  print cmd
+  print (cmd)
   status, output = commands.getstatusoutput(cmd)
   if status:
     print("Could not build FEC data! Error: %s" % output)
@@ -191,7 +191,7 @@ def BuildVerityMetadata(image_size, verity_metadata_path, root_hash, salt,
                         block_device, signer_path, key)
   if signer_args:
     cmd += " --signer_args=\"%s\"" % (' '.join(signer_args),)
-  print cmd
+  print (cmd)
   runcmd = ["system/extras/verity/build_verity_metadata.py", image_size, verity_metadata_path, root_hash, salt, block_device, signer_path, key];
   if verity_key_password is not None:
     sp = subprocess.Popen(runcmd, stdin=subprocess.PIPE)
