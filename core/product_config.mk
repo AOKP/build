0787410a686d52ab71aaef6debe3af43e7f4ae59
STELIX <ssspinni@gmail.com>
2017-11-09 16:56:35 +0100

    build - AOKPization [WIP]
    Change-Id: I164e6986dbf197c0e4fb9b9b28d01a194e453643
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A AOKP build needs only the AOKP product makefiles.
+ifneq ($(AOKP_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(AOKP_BUILD)/aokp.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # AOKP_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(AOKP_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
