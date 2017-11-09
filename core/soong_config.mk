2cc98d4df122f88ab88bc918237ce907dc00504e
STELIX <ssspinni@gmail.com>
2017-11-09 16:56:35 +0100

    build - AOKPization
    Change-Id: I164e6986dbf197c0e4fb9b9b28d01a194e453643
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -16,8 +16,8 @@ endif
 # Create soong.variables with copies of makefile settings.  Runs every build,
 # but only updates soong.variables if it changes
 SOONG_VARIABLES_TMP := $(shell mktemp -u)
-include vendor/lineage/build/soong/soong_config.mk
-$(SOONG_VARIABLES): FORCE lineage_soong
+include vendor/aokp/build/soong/soong_config.mk
+$(SOONG_VARIABLES): FORCE aokp_soong
 	$(hide) (\
 	echo '    "Make_suffix": "-$(TARGET_PRODUCT)",'; \
 	echo ''; \
