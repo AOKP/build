7a5bea63d61dfbaa5951dd628922a9a00e078d40
Ethan Chen <intervigil@gmail.com>
2018-05-20 14:38:31 -0700

    build: Clean up makefile hook inclusions
    * Include MTK/QCOM rules directly from
      vendor/aokp/build/core/config.mk hook, since that is what it is
      there for.
    * Remove apicheck.mk hook, this is unused, and if it turns out to be
      needed again, it can be included by the config.mk hook mentioned above.
    * Place main.mk hook into vendor/aokp/build/core/main.mk with the
      rest of the make build system hooks.
    
    Change-Id: I51bda9f5d35a63113d6eab98e06da8919ba46480

--- core/main.mk
+++ core/main.mk
@@ -128,10 +128,6 @@ else
   endif
 endif
 
-#
-# -----------------------------------------------------------------
-# Add Lineage generic properties to the build properties.
-ADDITIONAL_BUILD_PROPERTIES += $(PRODUCT_GENERIC_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk
@@ -331,7 +327,7 @@ ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.stack-trace-dir=/data/anr
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/aokp/configs/main.mk
+-include vendor/aokp/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
